3be0a1ea957aa591fd3d9884d5712340e3b7f7fd
tanmayjbpal <tanmayjbpal@gmail.com>
2025-07-17 15:52:29 +0530

    Recaptcha Changes

--- project.js
+++ project.js
@@ -16,3 +16,15 @@ function beforesubmit(){
 
 //Now this form is available in our Local Server. We want this form on the Web/Internat
 //so we are Using Git Pages for this. 
+
+//Adding From Genarate HTMl Form for reCAPTCHA purpose
+
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    } 
+} 
+setInterval(timestamp, 500); 
